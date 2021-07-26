0507f2004d9999da0a3c4e4d53eff60fd9baca49
Balu-ust <87757975+Balu-ust@users.noreply.github.com>
2021-07-26 12:16:26 +0530

    Add files via upload

---
+++ mysql/Sample.sql
@@ -0,0 +1,16 @@
+create database Sample;
+use Sample;
+
+create table emp(
+id int(10),
+name varchar(45),
+age int(3));
+
+insert into emp values(1,"ABC",25);
+
+select * from emp;
+
+insert into emp values(2,"hari",28);
+
+select* from  emp;
+
